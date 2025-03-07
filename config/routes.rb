3c1a56614f2a71a774aeeface32f04ec892168fb
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2025-03-07 06:13:01 -0300

    Adicionando rotas e a containerização

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  post '/urls', to: 'urls#create'
+  get '/:short_url', to: 'urls#show'
+  get '/:short_url/stats', to: 'urls#stats'
 end
